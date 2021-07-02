9943ffd5f4cbdecec26a5f0789d7549e5a5b34a1
Chris Lee <chris.lee@revature.net>
2021-07-02 04:06:27 -0700

    comit

---
+++ House.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HouseFunction
+public class NewBaseType
+{
+}
+
+public class House
+    {   
+        //This is a constructor (special method that is called upon when an object is initialized)
+        public House()
+        {
+            Console.WriteLine("That constructor of this class was used.");         
+        }
+    }
+
+    
